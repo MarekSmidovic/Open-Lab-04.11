0c65528035ba734c6c0e2cc9c95e78ff6afab731
smidovic <msmidovic@gmail.com>
2021-11-22 21:12:29 +0100

    podla abecedy

--- Open-Lab-04.11/StringTools.cs
+++ Open-Lab-04.11/StringTools.cs
@@ -6,7 +6,23 @@ namespace Open_Lab_04._11
     {
         public string AlphabetSoup(string str)
         {
-            throw new NotImplementedException();
+            char temp;
+            str = str.ToLower();
+            char[] charstr = str.ToCharArray();
+            for (int i = 1; i < charstr.Length; i++)    // for(int i=1;i< str.Length;i++)
+            {
+                for (int j = 0; j < charstr.Length - 1; j++)  //for(int j=0;j< str.Length-1;j++)
+                {
+                    if (charstr[j] > charstr[j + 1])
+                    {
+                        temp = charstr[j];
+                        charstr[j] = charstr[j + 1];
+                        charstr[j + 1] = temp;
+                    }
+                }
+            }
+            string a = charstr.ToString();
+            return a;
         }
     }
 }
